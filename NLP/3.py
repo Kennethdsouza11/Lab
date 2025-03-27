e6caa5650df8368e351b23501c0548c51fde9a29
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2025-03-27 22:06:21 +0530

    done

--- NLP/3.py
+++ NLP/3.py
@@ -1,2 +1,11 @@
 # WRITE A PROGRAM TO SPLIT A WORD INTO PAIR’S AT ALL POSSIBLE POSITIONS. FOR EXAMPLE, CARRIED WILL BE SPLIT INTO {C, ARRIED, CA ,RRIED, CAR, RIED, CARR, IED, CARRI, ED, CARRI, D}.
 
+
+def split_words(word):
+    pairs = [(word[:i], word[i:]) for i in range(1, len(word))]
+    return pairs
+
+
+word = "CARRIED"
+result = split_words(word)
+print(result)
