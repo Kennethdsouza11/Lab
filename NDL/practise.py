8386dbd2fa6ed0a38ac9ce9aa9bf748b26585231
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2025-04-22 16:41:40 +0530

    done

--- NDL/practise.py
+++ NDL/practise.py
@@ -5,7 +5,7 @@ from minisom import MiniSom
 np.random.seed(0)
 X = np.random.rand(100,2)
 
-som = MiniSom(x = 10, y = 10, input_len = 2, sigma = 1.0, learning_rate = 0.5)
+som = MiniSom(x = 10, y = 10, learning_rate = 0.5, sigma = 1.0, input_len =2)
 som.random_weights_init(X)
 som.train_random(X, num_iteration = 500)
 
@@ -16,7 +16,7 @@ plt.title('SOM')
 
 for i, x in enumerate(X):
     winner = som.winner(x)
-    plt.plot(winner[0] + 0.5, winner[1]+0.5, 'o', markerfacecolor = 'None', markeredgecolor = 'black', markersize = 8, markeredgewidth = 1)
+    plt.plot(winner[0]+0.5, winner[1]+0.5, 'o', markerfacecolor = 'None', markeredgecolor = 'black', markersize = 8, markeredgewidth = 1)
     
 plt.grid(True)
 plt.show()
